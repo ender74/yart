d97d481a7b1fee6b223ed578e39cf245bd70efb4
Heiko Hüter <heiko.hueter@ender74.de>
2016-03-21 17:38:21 +0100

    logical order of functions

--- src/views/app/login/actions/AuthActions.js
+++ src/views/app/login/actions/AuthActions.js
@@ -3,37 +3,6 @@ import Parse from 'parse'
 import C from './AuthConstants'
 
 const AuthActions = {
-    loginSucceeded(user) {
-        return {
-            type: C.AUTH_LOGIN_SUCCEDED,
-            user: user
-        }    
-    },
-        
-    logout() {
-        return {
-            type: C.AUTH_LOGOUT
-        }
-    },
-    
-    signUpFailed(user, code, error) {
-        return {
-            type: C.AUTH_SIGNUP_FAILED,
-            user: user,
-            code: code,
-            error: error
-        }
-    },
-    
-    loginFailed(user, code, error) {
-        return {
-            type: C.AUTH_LOGIN_FAILED,
-            user: user,
-            code: code,
-            error: error
-        }
-    },
-    
     loginBasic(username, password) {
         return dispatch => {
             Parse.User.logIn(username, password, {
@@ -46,7 +15,29 @@ const AuthActions = {
             })            
         }
     },
+
+    loginSucceeded(user) {
+        return {
+            type: C.AUTH_LOGIN_SUCCEDED,
+            user: user
+        }    
+    },
+        
+    loginFailed(user, code, error) {
+        return {
+            type: C.AUTH_LOGIN_FAILED,
+            user: user,
+            code: code,
+            error: error
+        }
+    },
     
+    logout() {
+        return {
+            type: C.AUTH_LOGOUT
+        }
+    },
+
     signUp(username, password, email) {
         return dispatch => {
             var user = new Parse.User();
@@ -63,7 +54,16 @@ const AuthActions = {
                 }
             })
         }
-    }    
+    },    
+
+    signUpFailed(user, code, error) {
+        return {
+            type: C.AUTH_SIGNUP_FAILED,
+            user: user,
+            code: code,
+            error: error
+        }
+    }
 }
 
 export default AuthActions
