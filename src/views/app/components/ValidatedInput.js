1d217c17fac8869674f7632a310f9c1644b8a9c9
Heiko Hüter <ender@ender74.de>
2016-03-30 23:30:02 +0200

    fixed date picker

--- src/views/app/components/ValidatedInput.js
+++ src/views/app/components/ValidatedInput.js
@@ -5,7 +5,10 @@ import ErrorDecorator from './ErrorDecorator'
 class ValidatedInput extends Component {
     render() {
         const { error, touched } = this.props
-        return <ErrorDecorator error={ error } touched={ touched }><Input {...this.props} /></ErrorDecorator>
+        const newProps = Object.assign({}, this.props)
+        if (!newProps.type)
+            newProps.type='text'
+        return <ErrorDecorator error={ error } touched={ touched }><Input {...newProps} /></ErrorDecorator>
     }
 }
 
