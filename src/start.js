280c42bada9715451410f7f7be9abda4cea1c7b3
Heiko Hüter <heiko.hueter@ender74.de>
2016-04-16 00:15:45 +0200

    added moment locale init

--- src/start.js
+++ src/start.js
@@ -2,9 +2,13 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import moment from 'moment'
 import Parse from 'parse'
 
 import AppView from './views/app/AppView'
+
+const locale = window.navigator.userLanguage || window.navigator.language
+require('moment/locale/de')
 
 Parse.initialize('www.log84.de');
 Parse.serverURL = '../parse'
