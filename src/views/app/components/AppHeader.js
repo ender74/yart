5521792cc235cd8b7b5acd15a829e4cce2f30e80
Heiko Hüter <ender@ender74.de>
2016-04-20 23:32:18 +0200

    collapse navbar after item action triggered

--- src/views/app/components/AppHeader.js
+++ src/views/app/components/AppHeader.js
@@ -2,52 +2,70 @@ import React, { Component } from 'react'
 import { PageHeader, Navbar, Nav, NavItem, NavDropdown, MenuItem, Glyphicon } from 'react-bootstrap'
 import { FormattedMessage, injectIntl } from 'react-intl'
 
-const AppHeader = ({ user, logout, setLocale, intl }) => {
-    var logout
-    if (user)
-        logout = (
-            <Nav pullRight>
-                <NavItem onClick={ logout }><Glyphicon glyph='log-out' />
-                    <FormattedMessage
-                        id='logout'
-                        defaultMessage='Logout'
-                    />
-                </NavItem>
-            </Nav>
-        )
-    const language = intl.formatMessage({
-        id: 'app.language',
-        defaultMessage: 'Language'
-    })
-    return (
-        <Navbar fixedTop fluid>
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <a href="https://www.log84.de">Log84.de</a>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-                <Nav>
-                    <NavDropdown eventKey={1} title={ language } id='select-language'>
-                        <MenuItem eventKey={1.1} onClick={ () => setLocale('en') }>
-                            <FormattedMessage
-                                id='app.language_en'
-                                defaultMessage='English'
-                            />
-                        </MenuItem>
-                        <MenuItem eventKey={1.2} onClick={ () => setLocale('de') }>
-                            <FormattedMessage
-                                id='app.language_de'
-                                defaultMessage='German'
-                            />
-                        </MenuItem>
-                    </NavDropdown>
+class AppHeader extends Component {
+    constructor() {
+        super()
+        this.state = {
+            expanded: false
+        }
+    }
+
+
+    render() {
+        const { user, logout, setLocale, intl } = this.props
+
+        var logoutItem
+        if (user)
+            logoutItem = (
+                <Nav pullRight>
+                    <NavItem onClick={ logout }><Glyphicon glyph='log-out' />
+                        <FormattedMessage
+                            id='logout'
+                            defaultMessage='Logout'
+                        />
+                    </NavItem>
                 </Nav>
-                {logout}
-            </Navbar.Collapse>
-        </Navbar>
-    )
+            )
+        const language = intl.formatMessage({
+            id: 'app.language',
+            defaultMessage: 'Language'
+        })
+        const mySetLocale = (locale) => {
+            this.setState({
+                expanded: false
+            })
+            setLocale(locale)
+        }
+        return (
+            <Navbar fixedTop fluid expanded={this.state.expanded} onToggle={(expanded) => this.setState({expanded: expanded})}>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="https://www.log84.de">Log84.de</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavDropdown eventKey={1} title={ language } id='select-language'>
+                            <MenuItem eventKey={1.1} onClick={ () => mySetLocale('en') }>
+                                <FormattedMessage
+                                    id='app.language_en'
+                                    defaultMessage='English'
+                                />
+                            </MenuItem>
+                            <MenuItem eventKey={1.2} onClick={ () => mySetLocale('de') }>
+                                <FormattedMessage
+                                    id='app.language_de'
+                                    defaultMessage='German'
+                                />
+                            </MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                    {logoutItem}
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
 }
 
 export default injectIntl(AppHeader)
