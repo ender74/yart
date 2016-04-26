783dd25fea539071cabbe7e36241793861d7db6a
Heiko Hüter <heiko.hueter@ender74.de>
2016-04-26 15:51:41 +0200

    replace custom checkstatus with npm module

--- src/views/app/actions/LocaleActions.js
+++ src/views/app/actions/LocaleActions.js
@@ -1,16 +1,7 @@
 import 'whatwg-fetch'
-
+import checkStatus from 'fetch-check-http-status'
 import { update } from 'react-intl-redux'
 
-const checkStatus = (response) => {
-    if (response.status >= 200 && response.status < 300) {
-        return response
-    } else {
-        var error = new Error(response.statusText)
-        error.response = response
-        throw error
-    }
-}
 
 const setLocale = (locale) => {
     return (dispatch) => {
