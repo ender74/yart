c585e961823fcef1acf2d1880ac906fcafa5d8fb
Heiko Hüter <heiko.hueter@ender74.de>
2016-05-14 17:33:16 +0200

    tododetails with fixed position

--- src/views/app/todos/BoundTodosView.js
+++ src/views/app/todos/BoundTodosView.js
@@ -9,24 +9,27 @@ import TodoToggleShowAll from './components/TodoToggleShowAll'
 import BoundTodos from './BoundTodos'
 import BoundTodoDetails from './BoundTodoDetails'
 
-const TodosView = ( { activeTodo, defaultChecked, toggleActive, toggleShowAll, saveChanges, update } ) => {
-    const detailsVisible = typeof activeTodo != 'undefined' && activeTodo.id
-    var col1Styles = detailsVisible ? { xsHidden: true, sm: 8 } : { sm: 12 }
-    var col2Styles = detailsVisible ? { sm: 4 } : {style: {'display': 'none'}}
-    return (
-        <Grid>
-            <Row>
-                <Col {...col1Styles}>
-                    <BoundTodos>
-                        <TodoToggleShowAll defaultChecked = { defaultChecked } toggleShowAll = { toggleShowAll } />
-                    </BoundTodos>
-                </Col>
-                <Col {...col2Styles}>
-                    <BoundTodoDetails todo = { activeTodo } />
-                </Col>
-            </Row>
-        </Grid>
-    )
+class TodosView extends Component {
+    render() {
+        const { activeTodo, defaultChecked, toggleActive, toggleShowAll, saveChanges, update } = this.props
+        const detailsVisible = typeof activeTodo != 'undefined' && activeTodo.id
+        var col1Styles = detailsVisible ? { xsHidden: true, sm: 8 } : { sm: 12 }
+        var col2Styles = detailsVisible ? { sm: 4 } : {style: {'display': 'none'}}
+        return (
+            <Grid>
+                <Row>
+                    <Col {...col1Styles}>
+                        <BoundTodos>
+                            <TodoToggleShowAll defaultChecked = { defaultChecked } toggleShowAll = { toggleShowAll } />
+                        </BoundTodos>
+                    </Col>
+                    <Col {...col2Styles}>
+                        <BoundTodoDetails todo = { activeTodo } parent = { this }/>
+                    </Col>
+                </Row>
+            </Grid>
+        )
+    }
 }
 
 function mapStateToProps(state) {
--- src/views/app/todos/components/TodoDetails.js
+++ src/views/app/todos/components/TodoDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {reduxForm} from 'redux-form'
 import { Form, ButtonToolbar, Button, Glyphicon } from 'react-bootstrap'
+import { AutoAffix } from 'react-overlays'
 import { FormField, DateField } from 'redux-form-fields'
 import { FormattedMessage, injectIntl } from 'react-intl'
 
@@ -38,50 +39,52 @@ const glyphiconBack = <Glyphicon glyph='backward' />
 class TodoDetails extends Component {
     render() {
         const onSubmit = (values) => this.props.onUpdate(this.props.todo, values)
-        const {fields: {text, url, due, location}, tags, onAddTag, onRemoveTag, handleSubmit, intl} = this.props
+        const {parent, fields: {text, url, due, location}, tags, onAddTag, onRemoveTag, handleSubmit, intl} = this.props
         const placeholder = intl.formatMessage({
             id: 'todo.add_tag',
             defaultMessage: '+ add new tag here'
         })
         return (
-            <form>
-                <FormField
-                    {...text} />
-                <FormField
-                    placeholder='http://www.log84.de'
-                    {...url} />
-                <DateField
-                    placeholder='17.03.2016'
-                    {...due} />
-                <FormField
-                    placeholder='Panoramastraße 1A, 10178 Berlin'
-                    {...location} />
-                <Tags tags = { tags } onRemoveTag = { onRemoveTag }/>
-                <FormField
-                    placeholder={ placeholder }
-                    onKeyDown = { ( event ) => {
-                            if (event.keyCode === ENTER_KEY_CODE) {
-                                onAddTag(event.target.value)
+            <AutoAffix viewportOffsetTop={70} container={parent} autoWidth={true}>
+                <form>
+                    <FormField
+                        {...text} />
+                    <FormField
+                        placeholder='http://www.log84.de'
+                        {...url} />
+                    <DateField
+                        placeholder='17.03.2016'
+                        {...due} />
+                    <FormField
+                        placeholder='Panoramastraße 1A, 10178 Berlin'
+                        {...location} />
+                    <Tags tags = { tags } onRemoveTag = { onRemoveTag }/>
+                    <FormField
+                        placeholder={ placeholder }
+                        onKeyDown = { ( event ) => {
+                                if (event.keyCode === ENTER_KEY_CODE) {
+                                    onAddTag(event.target.value)
+                                }
                             }
                         }
-                    }
-                />
-                <ButtonToolbar>
-                    <Button bsStyle='primary' onClick={ () => this.props.onClose( this.props.todo ) }>
-                        {glyphiconBack}
-                        <FormattedMessage
-                            id='todo.close'
-                            defaultMessage='Back'
-                        />
-                    </Button>
-                    <Button bsStyle='success' onClick={ handleSubmit(onSubmit) }>
-                        <FormattedMessage
-                            id='todo.save'
-                            defaultMessage='Save'
-                        />
-                    </Button>
-                </ButtonToolbar>
-            </form>
+                    />
+                    <ButtonToolbar>
+                        <Button bsStyle='primary' onClick={ () => this.props.onClose( this.props.todo ) }>
+                            {glyphiconBack}
+                            <FormattedMessage
+                                id='todo.close'
+                                defaultMessage='Back'
+                            />
+                        </Button>
+                        <Button bsStyle='success' onClick={ handleSubmit(onSubmit) }>
+                            <FormattedMessage
+                                id='todo.save'
+                                defaultMessage='Save'
+                            />
+                        </Button>
+                    </ButtonToolbar>
+                </form>
+            </AutoAffix>
         )
     }
 }
