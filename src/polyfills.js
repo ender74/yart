4f31969cc311196afc6bba7a969c7c9bc305059f
Heiko Hüter <heiko.hueter@ender74.de>
2016-05-26 15:25:42 +0200

    polyfill for Array.includes

--- src/polyfills.js
+++ src/polyfills.js
@@ -36,6 +36,40 @@ function polyfillIntl() {
     }
 }
 
+//needed for Edge / Internet Explorer
+
+function polyfillIncludes() {
+    if (!Array.prototype.includes) {
+        Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
+            'use strict';
+            var O = Object(this);
+            var len = parseInt(O.length) || 0;
+            if (len === 0) {
+                return false;
+            }
+            var n = parseInt(arguments[1]) || 0;
+            var k;
+            if (n >= 0) {
+                k = n;
+            } else {
+                k = len + n;
+                if (k < 0) {k = 0;}
+            }
+            var currentElement;
+            while (k < len) {
+                currentElement = O[k];
+                if (searchElement === currentElement ||
+                (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+                    return true;
+                }
+                k++;
+            }
+            return false;
+        };
+    }
+}
+
+
 function polyfillPromise() {
     require('es6-promise').polyfill()
 }
@@ -44,4 +78,5 @@ export default () => {
     polyfillPromise()
     polyfillAssign()
     polyfillIntl()
+    polyfillIncludes()
 }
